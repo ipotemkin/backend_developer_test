5e63787fa0bf4fd84877d1cfe1eb13b1f3a8e11c
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-04-14 07:40:24 +0300

    added my comments to README

--- src/database/tables.py
+++ src/database/tables.py
@@ -15,7 +15,7 @@ stats = sa.Table(
     metadata,
     sa.Column('id', sa.BigInteger, primary_key=True),  # TODO add comments on altering repo_id to id
     sa.Column('user_id', sa.BigInteger, sa.ForeignKey('user.id'), nullable=False),  # TODO add cascade
-    sa.Column('repo_id', sa.BigInteger, nullable=True),  # TODO add comments on altering repo_id to id
+    sa.Column('repo_id', sa.BigInteger, nullable=False),  # TODO add comments on altering repo_id to id
     sa.Column('date', sa.Date, nullable=False),
     sa.Column('stargazers', sa.Integer, nullable=False),
     sa.Column('forks', sa.Integer, nullable=False),
