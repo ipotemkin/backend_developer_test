4b72b142f20b68e224dff9a1a49c465d74749ccb
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-04-14 06:59:07 +0300

    added error handlers

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,12 +1,19 @@
-import sqlalchemy as sa
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi_utils.tasks import repeat_every
 
-from src.api import users, protocols, stats
-from src.database import DatabaseSettings, create_database_url
-from src.dependencies import get_engine
 from src.github_api import get_github_repos_by_login, update_stats
 
+import src.api.protocols
+from src.api import users, protocols, stats
+from src.dependencies import get_engine
+from src.errors import (
+    NotFoundError,
+    NoContentError,
+    DatabaseError,
+    BadRequestError,
+    ValidationError
+)
 from src.stats.service import StatService
 from src.user.service import UserService
 
@@ -48,3 +55,30 @@ async def on_startup():
     for user in _users:
         repos = await get_github_repos_by_login(user.login)
         update_stats(user.id, repos, stat_service)
+
+
+# exception handlers
+@app.exception_handler(404)
+@app.exception_handler(NotFoundError)
+def not_found_error(request: Request, exc: NotFoundError):
+    return JSONResponse(status_code=404, content={"message": "Not Found"})
+
+
+@app.exception_handler(NoContentError)
+def no_content_error(request: Request, exc: NoContentError):
+    return JSONResponse(status_code=204, content={"message": "No Content"})
+
+
+@app.exception_handler(DatabaseError)
+def database_error(request: Request, exc: DatabaseError):
+    return JSONResponse(status_code=400, content={"message": "Database Error"})
+
+
+@app.exception_handler(BadRequestError)
+def bad_request_error(request: Request, exc: BadRequestError):
+    return JSONResponse(status_code=400, content={"message": "Bad Request"})
+
+
+@app.exception_handler(ValidationError)
+def validation_error(request: Request, exc: ValidationError):
+    return JSONResponse(status_code=400, content={"message": "Validation Error"})
